066626910e454778e129e9a6828a6dc416c82772
Casey6624 <34159052+Casey6624@users.noreply.github.com>
2019-10-27 17:46:52 +0000

    Implemented controls and basic slide transitions

--- app/components/Scene.js
+++ app/components/Scene.js
@@ -1,7 +1,8 @@
 import React from "react";
 //import Left from "../../assets/images/back.svg";
 //import Right from "../../assets/images/next.svg";
-import LeftArrow from "./Left";
+//import Icons from "../../assets/images/Icons";
+import { Back } from "../../assets/images/Icons";
 
 const defaultImage =
   "https://www.google.com/url?sa=i&source=images&cd=&ved=2ahUKEwie-KWs3rDlAhUb4OAKHRODB8AQjRx6BAgBEAQ&url=https%3A%2F%2Fwww.nationalpetregister.org%2Fmp.php%3Fpt%3DBird&psig=AOvVaw2W94vDitDWnsGqMOY7jr5K&ust=1571863577160949";
@@ -12,7 +13,6 @@ export default function Scene({ imageUrl = defaultImage, overlayText = "" }) {
       className="sceneContainer"
       style={{ backgroundImage: `url(${imageUrl})` }}
     >
-      <LeftArrow />
       <h1 className="overlayText">{overlayText}</h1>
     </div>
   );
--- app/containers/Shortcode.jsx
+++ app/containers/Shortcode.jsx
@@ -1,20 +1,85 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import Scene from "../components/Scene";
 
-export default class Shortcode extends Component {
-  render() {
-    return (
-      <div className="Merry-Go-Container">
-        <Scene
-          imageUrl={
-            "https://cdn.sandals.com/beaches/v12/images/general/destinations/home/beach.jpg"
-          }
-          overlayText="Scene One"
-        />
-      </div>
-    );
+export default function Shortcode(props) {
+  const [scenes, setScenes] = useState(null);
+  const [currSlide, setCurrSlide] = useState(0);
+
+  useEffect(() => {
+    setScenes([
+      {
+        imageUrl:
+          "https://cdn.sandals.com/beaches/v12/images/general/destinations/home/beach.jpg",
+        overlayText: "Scene One"
+      },
+      {
+        imageUrl:
+          "https://images.pexels.com/photos/414171/pexels-photo-414171.jpeg",
+        overlayText: "Scene Two"
+      },
+      {
+        imageUrl:
+          "https://www.nationalgeographic.com/content/dam/photography/photos/000/000/6.ngsversion.1467942028599.adapt.1900.1.jpg",
+        overlayText: "Scene Three"
+      },
+      {
+        imageUrl:
+          "https://i2.wp.com/www.globallandscapesforum.org/wp-content/uploads/2017/10/What-are-landscape_.jpg",
+        overlayText: "Scene Four"
+      }
+    ]);
+  }, []);
+
+  useEffect(() => {
+    if (!scenes) return;
+    console.log(currSlide);
+    console.log(scenes[currSlide]);
+  }, [currSlide]);
+
+  function handleSlideChange({ target }) {
+    const { name } = target;
+    if (name === "back") {
+      if (currSlide === 0) {
+        setCurrSlide(scenes.length);
+      }
+      setCurrSlide(currSlide - 1);
+      return;
+    }
+    if (name === "next") {
+      if (currSlide === scenes.length + 1) {
+        setCurrSlide(0);
+      }
+      setCurrSlide(currSlide + 1);
+    }
   }
+
+  if (!scenes) return <div>Loading...</div>;
+
+  return (
+    <div className="Merry-Go-Container">
+      <Scene
+        imageUrl={scenes[currSlide].imageUrl}
+        overlayText={scenes[currSlide].overlayText}
+      />
+      <div className="controlsContainer">
+        <button
+          className="controls"
+          name="back"
+          onClick={e => handleSlideChange(e)}
+        >
+          Back
+        </button>
+        <button
+          className="controls"
+          name="next"
+          onClick={e => handleSlideChange(e)}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
 
 Shortcode.propTypes = {
